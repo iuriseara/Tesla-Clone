6d3d52f2cd757d84bb2f06bf7b7dbd550066aa7b
iuriseara <iuriseara@gmail.com>
2021-05-27 14:48:57 -0400

    Added Props to Home Page & Styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,54 @@ import Section from './Section'
 function Home() {
     return (
         <Container>
-            <Section />
+             <Section
+                title="Model S"
+                description="Order Online for"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model 3"
+                description="Order Online for"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model X"
+                description="Order Online for"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order Online for"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Lowest Cost Solar Panels in America"
+                description="Money-back Guarantee"
+                backgroundImg="solar-panels.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn more"
+            />
+            <Section
+                title="Solar for new Roofs"
+                description="Solar Roofs Costs Less Than a New Roof Plus Solar Panels"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn more"
+            />
+            <Section
+                title="Accessories"
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop Now"
+            />
         </Container>
     )
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Section() {
+function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
     return (
-        <Wrap>
-            Section
+        <Wrap bgImage={backgroundImg}>
+            <ItemText>
+                <h1>{ title }</h1>
+                <p>{ description } <a href="#">Touchless Delivery</a></p>
+            </ItemText>
+
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        {leftBtnText}
+                    </LeftButton>
+
+                    <RightButton>
+                        {rightBtnText}
+                    </RightButton>
+
+                </ButtonGroup>
+                    <DownArrow src="../../img/arrow.svg" />
+            </Buttons>
+
         </Wrap>
     )
 }
@@ -12,5 +30,65 @@ function Section() {
 export default Section
 
 const Wrap = styled.div`
-
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('../../img/model-s.jpg');
+    background-image: ${props => `url("/img/${props.bgImage}")`};
+    display: flex;
+    flex-direction: column; 
+    justify-content: space-between;  
+    align-items: center;   
 `
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+  
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+`
+
+const LeftButton = styled.button`
+    border: none;
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px
+`
+
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    opacity: 0.65;
+    color: black;
+`
+
+const DownArrow = styled.img`
+    height: 25px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
+`
+
+const Buttons = styled.div`
+    margin-bottom: 2%;
+`
+// const Underline = styled.a`
+
+// `
